1651838e01f37ab2a018ad68a3123d6b74c28336
goutamgdy <91448655+goutamgdy@users.noreply.github.com>
2024-09-14 00:16:24 +0530

    Added

--- Assignments/Java/3-This & Static/BankAccountDemo.java
+++ Assignments/Java/3-This & Static/BankAccountDemo.java
@@ -1,6 +1,10 @@
 class BankAccount{
-    int acc_no;
-    double balance;
+    private int acc_no;
+    private double balance;
+    private static  float int_rate;
+    static{
+        int_rate = 4.5f;
+    }
     public BankAccount(){
         acc_no = 1000;
         balance = 10000; // Default, no args constructor
@@ -9,17 +13,31 @@ class BankAccount{
         this.acc_no = acc_no;
         this.balance = balance;
     }
-    public BankAccount(inr acc_no){
+    public BankAccount(int acc_no){
         this(acc_no, 10000);
     }
     public void display(){
-        System.out.println("Account: " + acc_no + " Balance: " + balance);
+        System.out.println("Account: " + acc_no + " Balance without intrest: " + balance);
+    }
+    public double calcInterestRate(){
+        balance = (float) balance; // type parsing in java - Integer.parseInt(String)=> string to int
+        return balance + (( balance * int_rate) / 100); 
+    }
+    public static void updateInterestRate(float rate){ // Why cant be made as private...?
+        int_rate = rate;
+    }
+    
+}
+
+class BankAccountDemo{
+    public static void main(String[] args){
+        BankAccount b1 = new BankAccount(1001, 20000);
+        b1.calcInterestRate();
+        // b1.display();
+        System.out.println("Before updating interest balance: "  + b1.calcInterestRate());
+        BankAccount.updateInterestRate(4.7f);
+        System.out.println("After updating interest, Balance: " + b1.calcInterestRate());
+        // b1.display();
     }
 }
 
-class BankAccountDemo(){
-    public static void main(String[] args){
-        BankAccount b1(1001, 20000);
-        b1.display();
-    }
-}
