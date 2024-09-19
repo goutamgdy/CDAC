38b080f705b74fa18df9e2ce8ed9d494c1f1523d
goutamgdy <91448655+goutamgdy@users.noreply.github.com>
2024-09-19 22:37:11 +0530

    Addded

--- Assignments/Java/1-Basics/BoxDemo.java
+++
@@ -1,8 +0,0 @@
-class BoxDemo{
-	public static void main(String[] args){
-		Box b1 = new Box();
-		b1.accept(1,1,1);
-		b1.display();
-		System.out.println("Volume: " + b1.volume());
-	}
-}
--- Assignments/Java/1-Basics/PointDemo.java
+++
@@ -1,7 +0,0 @@
-class PointDemo{
-	public static void main(String[] args){
-		Point p1 = new Point();
-		p1.accept(10, -12);
-		p1.display();
-	}
-}
